8018f0ebba310a95afdb458492491d3371a83f46
Peter <peter@Peters-MacBook-Pro.local>
2024-09-19 08:27:02 -0700

    updates to supported plate solver

--- examples/figures/convergence_figure.m
+++ examples/figures/convergence_figure.m
@@ -7,7 +7,7 @@ clear
 % 
 % zk = 0.1;               % our k (wave number)
 
-maxchunklens = [4 2 1 0.5 0.25 0.125 0.0625 0.03125];
+maxchunklens = [4 2 1 0.5 0.25  0.125 0.0625 ]; % 0.03125];
 npts = maxchunklens*0;
 free_errors = maxchunklens*0;
 clamped_errors = maxchunklens*0;
@@ -45,23 +45,25 @@ for i = 1:numel(npts)
     opts = [];
     opts.sing = 'log';
 
+    kappa = signed_curvature(chnkr);
+    kappa = kappa(:);
+
     % clamped plate
     
     opts = [];
     opts.sing = 'log';
     fkern =  @(s,t) flex2d.kern(zk, s, t, 'clamped-plate');           % build the desired kernel
-    
+
     start = tic;
     D = chunkermat(chnkr,fkern, opts);
     t1 = toc(start);
-    
+
     fprintf('%5.2e s : time to assemble matrix\n',t1)
-    
-    
-    
-    kappa = signed_curvature(chnkr);
-    kappa = kappa(:);
-    
+
+
+
+
+
     A = zeros(2, 2, chnkr.npt);
     start = tic;
     for j = 1:chnkr.npt
@@ -69,45 +71,45 @@ for i = 1:numel(npts)
     end
     t3 = toc(start); 
     fprintf('%5.2e s : time to construct jump matrix\n',t3);
-    
+
     K = num2cell(A, [1 2]);
     M = blkdiag(K{:}); 
-     
-    
+
+
     [y1, grad, ~, ~, ~] = flex2d.hkdiffgreen(zk, centre, chnkr.r);
-    
+
     nx = chnkr.n(1,:); 
     ny = chnkr.n(2,:);
-    
+
     normalderiv = grad(:, :, 1).*(nx.')+ grad(:, :, 2).*(ny.');                                % Dirichlet and Neumann BC(Clamped BC)                         
-    
+
     firstbc = 1/(2*zk^2).*y1;
     secondbc = 1/(2*zk^2).*normalderiv;
-    
+
     [nt, ~] = size(D);
     lhs = M + D;
-    
+
     rhs = zeros(nt, 1); rhs(1:2:end) = firstbc ; rhs(2:2:end) = secondbc;
-    
-    
+
+
     sol = lhs\rhs;
-    
+
     rho1 = sol(1:2:end);                                    % first density
     rho2 = sol(2:2:end);        
-    
-    
+
+
     ikern1 = @(s,t) flex2d.kern(zk, s, t, 'first kernel');                              % build the kernel of evaluation          
     ikern2 = @(s,t) flex2d.kern(zk, s, t, 'second kernel');
-    
-    
+
+
     start1 = tic;
     utarg = chunkerkerneval(chnkr, ikern1,rho1, targets) + chunkerkerneval(chnkr, ikern2, rho2, targets);
     t2 = toc(start1);
     fprintf('%5.2e s : time for kernel eval (for plotting)\n',t2)
 
-    
+
     [val, ~] = flex2d.hkdiffgreen(zk, centre, targets);
-    
+
     true_sol = 1/(2*zk^2).*val;
 
     uerr = utarg - true_sol;
@@ -116,131 +118,131 @@ for i = 1:numel(npts)
 
     % free plate
 
-    fkern =  @(s,t) flex2d.kern(zk, s, t, 'free plate first part', coefs);        % build the desired kernel
-    
-    
-    fkern1 = @(s,t) flex2d.kern(zk, s, t, 'free plate hilbert subtract', coefs);                   % hilbert subtraction kernels in K11
-    fkern2 = @(s,t) flex2d.kern(zk, s, t, 'free plate coupled hilbert', coefs);   
-    
-    hilbert = @(s,t) lap2d.kern(s, t, 'hilb');
-    double = @(s,t) lap2d.kern(s,t, 'd');
-    
-    fkern3 = @(s,t) flex2d.kern(zk, s, t, 'free plate K21 first part', coefs);                     % singularity subtration kernel in K21 (including swapping its Asmyptotics expansions)
-    
-    fkern4 = @(s,t) flex2d.kern(zk, s, t, 'free plate K21 second part', coefs);                    % kernels in K21 needs to multiply by curvature
-    
-    fkern5 = @(s,t) flex2d.kern(zk, s, t, 'free plate K21 hilbert part', coefs);                   % kernels in K21 coupled with hilbert transforms and needs to multiply by curvature
-    
-    fkern6 = @(s,t) flex2d.kern(zk, s, t, 'free plate K22 second part', coefs);                    % kernels in K22 needs to multiply by curvature
-    
-    
-    start = tic;
-    sysmat = chunkermat(chnkr,fkern, opts);
-    sysmat1 = chunkermat(chnkr, fkern1, opts);
-    sysmat2 = chunkermat(chnkr, fkern2, opts);
-    
-    K21 = chunkermat(chnkr, fkern3, opts);
-    
-    K21second = chunkermat(chnkr, fkern4, opts);
-    K21hilbert = chunkermat(chnkr, fkern5, opts);
-    
-    K22second = chunkermat(chnkr, fkern6, opts);
-    
-    D = chunkermat(chnkr, double, opts);
-    t1 = toc(start);
-    fprintf('%5.2e s : time to assemble matrix\n',t1)
-    
-    opts2 = [];
-    opts2.sing = 'pv';
-    
-    start = tic;
-    H = chunkermat(chnkr, hilbert, opts2);                                              % Assemble hilbert transforms
-    t1 = toc(start);
-    fprintf('%5.2e s : time to assemble matrix\n',t1)
-    
-    
-    
-    kappa = signed_curvature(chnkr);
-    kappa = kappa(:);
-    
-    
-    hilb = sysmat1*H - ((1+nu)/2).*(D*D)- ((1+nu)*nu/2).*(D*D);
-    hilb2 = sysmat2*H ;
-    
-    mat1 =  sysmat(1:2:end, 1:2:end);
-    mat4 =  sysmat(2:2:end, 2:2:end);
-    
-    
-    sysmat(1:2:end, 1:2:end) = mat1 + hilb;
-    sysmat(2:2:end, 1:2:end) = K21 +  hilb2 + (kappa).*(K21hilbert*H + K21second);
-    sysmat(2:2:end, 2:2:end) = mat4 + (kappa).*(K22second);
-    
-    
-    A = [-1/2 + (1/8)*(1+nu).^2, 0; 0, 1/2];                                     % jump matrix (for exterior problem)
-    
-    M = kron(eye(chnkr.npt), A);
-    
-    lhs =  M + sysmat;
-    
-    zkimag = (1i)*zk;
-    [~, ~, hess, third, ~] = flex2d.hkdiffgreen(zk, centre, chnkr.r);
-    
-    nx = chnkr.n(1,:).'; 
-    ny = chnkr.n(2,:).';
-    
-    dx = chnkr.d(1,:).';
-    dy = chnkr.d(2,:).';
-    
-    ds = sqrt(dx.*dx+dy.*dy);
-    taux = (dx./ds); % normalization
-    tauy = (dy./ds);
-    
-
-    firstbc = 1/(2*zk^2).*(hess(:, :, 1).*(nx.*nx) + hess(:, :, 2).*(2*nx.*ny) + hess(:, :, 3).*(ny.*ny))+...
-               coefs(1)/(2*zk^2).*(hess(:, :, 1).*(taux.*taux) + hess(:, :, 2).*(2*taux.*tauy) + hess(:, :, 3).*(tauy.*tauy));
-    
-
-    secondbc = 1./(2*zk^2).*(third(:, :, 1).*(nx.*nx.*nx) + third(:, :, 2).*(3*nx.*nx.*ny) +...
-           third(:, :, 3).*(3*nx.*ny.*ny) + third(:, :, 4).*(ny.*ny.*ny))+...
-            (2-coefs(1))/(2*zk^2).*(third(:, :, 1).*(taux.*taux.*nx) + third(:, :, 2).*(taux.*taux.*ny + 2*taux.*tauy.*nx) +...
-            third(:, :, 3).*(2*taux.*tauy.*ny+ tauy.*tauy.*nx) +...
-            + third(:, :, 4).*(tauy.*tauy.*ny))+...
-            (1-coefs(1)).*(kappa).*(1/(2*zk^2).*(hess(:, :, 1).*taux.*taux + hess(:, :, 2).*(2*taux.*tauy) + hess(:, :, 3).*tauy.*tauy)-...
-            (1/(2*zk^2).*(hess(:, :, 1).*nx.*nx + hess(:, :, 2).*(2*nx.*ny) + hess(:, :, 3).*ny.*ny)));
-    
-    [nt, ~] = size(sysmat);
-    
-    rhs = zeros(nt, 1); 
-    rhs(1:2:end) = firstbc ; 
-    rhs(2:2:end) = secondbc;
-    
-    sol = lhs\rhs;
-    
-    rho1 = sol(1:2:end);                                    % first density
-    rho2 = sol(2:2:end);        
-    
-    
-    ikern1 = @(s,t) flex2d.kern(zk, s, t, 'free plate eval first', coefs);                              % build the kernel of evaluation          
-    ikern2 = @(s,t) flex2d.kern(zk, s, t, 'free plate eval second');
-    ikern3 = @(s,t) flex2d.kern(zk, s, t, 'free plate eval first hilbert',coefs);
-    
-    coupled = chunkerkerneval(chnkr, ikern3, H*rho1, targets);
-    
-    
-    start1 = tic;
-    utarg = chunkerkerneval(chnkr, ikern1,rho1, targets) + coupled +chunkerkerneval(chnkr, ikern2, rho2, targets);
-    t2 = toc(start1);
-    fprintf('%5.2e s : time for kernel eval (for plotting)\n',t2)
-    
-
-    
-    [val,~] = flex2d.hkdiffgreen(zk,centre,targets);        % Hankel part
-    
-    true_sol = 1/(2*zk^2).*val ;
-        
-    uerr = utarg - true_sol;
-    uerr = uerr ./  max(abs(true_sol));
-    free_errors(i) = norm(uerr);
+    % fkern =  @(s,t) flex2d.kern(zk, s, t, 'free plate first part', coefs);        % build the desired kernel
+    % 
+    % 
+    % fkern1 = @(s,t) flex2d.kern(zk, s, t, 'free plate hilbert subtract', coefs);                   % hilbert subtraction kernels in K11
+    % fkern2 = @(s,t) flex2d.kern(zk, s, t, 'free plate coupled hilbert', coefs);   
+    % 
+    % hilbert = @(s,t) lap2d.kern(s, t, 'hilb');
+    % double = @(s,t) lap2d.kern(s,t, 'd');
+    % 
+    % fkern3 = @(s,t) flex2d.kern(zk, s, t, 'free plate K21 first part', coefs);                     % singularity subtration kernel in K21 (including swapping its Asmyptotics expansions)
+    % 
+    % fkern4 = @(s,t) flex2d.kern(zk, s, t, 'free plate K21 second part', coefs);                    % kernels in K21 needs to multiply by curvature
+    % 
+    % fkern5 = @(s,t) flex2d.kern(zk, s, t, 'free plate K21 hilbert part', coefs);                   % kernels in K21 coupled with hilbert transforms and needs to multiply by curvature
+    % 
+    % fkern6 = @(s,t) flex2d.kern(zk, s, t, 'free plate K22 second part', coefs);                    % kernels in K22 needs to multiply by curvature
+    % 
+    % 
+    % start = tic;
+    % sysmat = chunkermat(chnkr,fkern, opts);
+    % sysmat1 = chunkermat(chnkr, fkern1, opts);
+    % sysmat2 = chunkermat(chnkr, fkern2, opts);
+    % 
+    % K21 = chunkermat(chnkr, fkern3, opts);
+    % 
+    % K21second = chunkermat(chnkr, fkern4, opts);
+    % K21hilbert = chunkermat(chnkr, fkern5, opts);
+    % 
+    % K22second = chunkermat(chnkr, fkern6, opts);
+    % 
+    % D = chunkermat(chnkr, double, opts);
+    % t1 = toc(start);
+    % fprintf('%5.2e s : time to assemble matrix\n',t1)
+    % 
+    % opts2 = [];
+    % opts2.sing = 'pv';
+    % 
+    % start = tic;
+    % H = chunkermat(chnkr, hilbert, opts2);                                              % Assemble hilbert transforms
+    % t1 = toc(start);
+    % fprintf('%5.2e s : time to assemble matrix\n',t1)
+    % 
+    % 
+    % 
+    % kappa = signed_curvature(chnkr);
+    % kappa = kappa(:);
+    % 
+    % 
+    % hilb = sysmat1*H - ((1+nu)/2).*(D*D)- ((1+nu)*nu/2).*(D*D);
+    % hilb2 = sysmat2*H ;
+    % 
+    % mat1 =  sysmat(1:2:end, 1:2:end);
+    % mat4 =  sysmat(2:2:end, 2:2:end);
+    % 
+    % 
+    % sysmat(1:2:end, 1:2:end) = mat1 + hilb;
+    % sysmat(2:2:end, 1:2:end) = K21 +  hilb2 + (kappa).*(K21hilbert*H + K21second);
+    % sysmat(2:2:end, 2:2:end) = mat4 + (kappa).*(K22second);
+    % 
+    % 
+    % A = [-1/2 + (1/8)*(1+nu).^2, 0; 0, 1/2];                                     % jump matrix (for exterior problem)
+    % 
+    % M = kron(eye(chnkr.npt), A);
+    % 
+    % lhs =  M + sysmat;
+    % 
+    % zkimag = (1i)*zk;
+    % [~, ~, hess, third, ~] = flex2d.hkdiffgreen(zk, centre, chnkr.r);
+    % 
+    % nx = chnkr.n(1,:).'; 
+    % ny = chnkr.n(2,:).';
+    % 
+    % dx = chnkr.d(1,:).';
+    % dy = chnkr.d(2,:).';
+    % 
+    % ds = sqrt(dx.*dx+dy.*dy);
+    % taux = (dx./ds); % normalization
+    % tauy = (dy./ds);
+    % 
+    % 
+    % firstbc = 1/(2*zk^2).*(hess(:, :, 1).*(nx.*nx) + hess(:, :, 2).*(2*nx.*ny) + hess(:, :, 3).*(ny.*ny))+...
+    %            coefs(1)/(2*zk^2).*(hess(:, :, 1).*(taux.*taux) + hess(:, :, 2).*(2*taux.*tauy) + hess(:, :, 3).*(tauy.*tauy));
+    % 
+    % 
+    % secondbc = 1./(2*zk^2).*(third(:, :, 1).*(nx.*nx.*nx) + third(:, :, 2).*(3*nx.*nx.*ny) +...
+    %        third(:, :, 3).*(3*nx.*ny.*ny) + third(:, :, 4).*(ny.*ny.*ny))+...
+    %         (2-coefs(1))/(2*zk^2).*(third(:, :, 1).*(taux.*taux.*nx) + third(:, :, 2).*(taux.*taux.*ny + 2*taux.*tauy.*nx) +...
+    %         third(:, :, 3).*(2*taux.*tauy.*ny+ tauy.*tauy.*nx) +...
+    %         + third(:, :, 4).*(tauy.*tauy.*ny))+...
+    %         (1-coefs(1)).*(kappa).*(1/(2*zk^2).*(hess(:, :, 1).*taux.*taux + hess(:, :, 2).*(2*taux.*tauy) + hess(:, :, 3).*tauy.*tauy)-...
+    %         (1/(2*zk^2).*(hess(:, :, 1).*nx.*nx + hess(:, :, 2).*(2*nx.*ny) + hess(:, :, 3).*ny.*ny)));
+    % 
+    % [nt, ~] = size(sysmat);
+    % 
+    % rhs = zeros(nt, 1); 
+    % rhs(1:2:end) = firstbc ; 
+    % rhs(2:2:end) = secondbc;
+    % 
+    % sol = lhs\rhs;
+    % 
+    % rho1 = sol(1:2:end);                                    % first density
+    % rho2 = sol(2:2:end);        
+    % 
+    % 
+    % ikern1 = @(s,t) flex2d.kern(zk, s, t, 'free plate eval first', coefs);                              % build the kernel of evaluation          
+    % ikern2 = @(s,t) flex2d.kern(zk, s, t, 'free plate eval second');
+    % ikern3 = @(s,t) flex2d.kern(zk, s, t, 'free plate eval first hilbert',coefs);
+    % 
+    % coupled = chunkerkerneval(chnkr, ikern3, H*rho1, targets);
+    % 
+    % 
+    % start1 = tic;
+    % utarg = chunkerkerneval(chnkr, ikern1,rho1, targets) + coupled +chunkerkerneval(chnkr, ikern2, rho2, targets);
+    % t2 = toc(start1);
+    % fprintf('%5.2e s : time for kernel eval (for plotting)\n',t2)
+    % 
+    % 
+    % 
+    % [val,~] = flex2d.hkdiffgreen(zk,centre,targets);        % Hankel part
+    % 
+    % true_sol = 1/(2*zk^2).*val ;
+    % 
+    % uerr = utarg - true_sol;
+    % uerr = uerr ./  max(abs(true_sol));
+    % free_errors(i) = norm(uerr);
 
     % supported plate
 
@@ -356,10 +358,19 @@ for i = 1:numel(npts)
     supported_errors(i) = norm(uerr);
 end
 
+%%
+
 figure(2)
-loglog(npts , clamped_errors, '.-', npts, free_errors, '.-', npts, supported_errors,'.-', MarkerSize=20)
+% loglog(npts , clamped_errors, '.-', npts, free_errors, '.-', npts, supported_errors,'.-', MarkerSize=20)
+loglog(npts, clamped_errors,'.-', MarkerSize=20)
+hold on 
+loglog(npts, 100*npts.^(-3),'-')
 hold on
-legend('clamped error','free error','supported plate')
+legend('supported plate','n^{-3}')
+% legend('clamped error','free error','supported plate')
 xlabel('N')
 ylabel('Relative error')
 title('Analytic solution test')
+
+% Integral of absolute value of density on boundary 
+% Maximum of density on boundary 
